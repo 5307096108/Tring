8fbd30029a2de69e0117382b6869de78351e31e6
5307096108 <126441716+5307096108@users.noreply.github.com>
2023-02-28 17:22:04 -0800

    Update GeClass.java
    integer process

--- src/ghclass/GeClass.java
+++ src/ghclass/GeClass.java
@@ -3,8 +3,9 @@ package ghclass;
 public class GeClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a = 12;
+		int b =27;
+		System.out.println(a+b);
 	}
 
 }
